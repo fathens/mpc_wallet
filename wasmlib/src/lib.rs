eed973d04a44e1d60c2d90c10a7e400fc5975c1a
沢谷 邦夫 <devel@fathens.org>
2022-05-05 15:00:10 +0900

    Add tasks and packages for flutter_rust_bridge

--- wasmlib/src/lib.rs
+++ wasmlib/src/lib.rs
@@ -1,24 +1,3 @@
+mod bridge_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
+mod api;
 mod utils;
-
-use wasm_bindgen::prelude::*;
-
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasmlib!");
-}
-
-#[wasm_bindgen]
-pub fn calc_add(a: u8, b: u8) -> u8 {
-    return a + b;
-}
